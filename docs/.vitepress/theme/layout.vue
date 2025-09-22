9bcbd436bbf7f5d5dfeda6bd14b1d2f92b857d43
unknown <wujingbo12l@outlook.com>
2025-09-22 20:24:18 +0800

    重新添加评论功能

---
+++ docs/.vitepress/theme/layout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import Twikoo from './Twikoo.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <Twikoo></Twikoo>
+    </template>
+  </Layout>
+</template>
+
+<style>
+.twikoo .el-loading-spinner .circular {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
